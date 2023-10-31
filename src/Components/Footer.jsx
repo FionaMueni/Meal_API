b742281a569a610c9c72c031fffb75afe5c1090b
FionaMueni <fionamueni7@gmail.com>
2023-10-31 15:38:22 +0300

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import facebookIcon from './../assets/Facebook_icon.png';
+import instagramIcon from './../assets/instagram_icon.png';
+import twitterIcon from './../assets/twittericon.png';
 
 const Footer = () => {
     return (
         <div>
-            <div className='bg-black justify-center items-center'>
+            <div className='flex flex-col bg-slate-950 justify-center items-center text-white'>
                <h1>Tasty Food Meals</h1>
-                <h2>Contact Us</h2>
+             
+               <span>Contact Us</span>
+               <img src={facebookIcon} className='w-12' alt=""/>
+               <img src={instagramIcon} className='w-12' alt=""/>
+               <img src={twitterIcon} className="w-12" alt=""/>
+
+    
+               <span>Copyright @2023</span>
+
+         
+                
             </div>
-            
+
             <div>
 
             </div>
